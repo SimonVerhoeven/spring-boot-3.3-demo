603c4eb7eb16b2eb73fa069e6234df0dc6c79254
Simon Verhoeven <verhoeven.simon@gmail.com>
2024-03-27 21:08:58 +0100

    feature: init repository

---
+++ src/main/java/dev/simonverhoeven/springboot33demo/SpringBoot33DemoApplication.java
@@ -0,0 +1,13 @@
+package dev.simonverhoeven.springboot33demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot33DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot33DemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/simonverhoeven/springboot33demo/SpringBoot33DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.simonverhoeven.springboot33demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot33DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
