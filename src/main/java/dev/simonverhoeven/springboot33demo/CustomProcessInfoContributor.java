cbc9fdb1e7b611f62d07da3b893c6f26abe6cabc
Simon Verhoeven <verhoeven.simon@gmail.com>
2024-04-03 20:00:42 +0200

    feature: enable info actuator to showcase processinfo

--- src/main/java/dev/simonverhoeven/springboot33demo/CustomProcessInfoContributor.java
+++ src/main/java/dev/simonverhoeven/springboot33demo/CustomProcessInfoContributor.java
@@ -1,2 +1,13 @@
-package dev.simonverhoeven.springboot33demo;public class CustomProcessInfoContributor {
+package dev.simonverhoeven.springboot33demo;
+
+import org.springframework.boot.actuate.info.Info;
+import org.springframework.boot.actuate.info.ProcessInfoContributor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomProcessInfoContributor extends ProcessInfoContributor {
+    @Override
+    public void contribute(Info.Builder builder) {
+        super.contribute(builder);
+    }
 }
